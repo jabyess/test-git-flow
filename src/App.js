b661856a0f4152b46575f1d16ddd705c99f8bf8f
James Byess <james.byess@gmail.com>
2019-05-17 16:21:39 -0400

    two commits now

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Something goes here!!!!!
+
+        I forgot something else
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
